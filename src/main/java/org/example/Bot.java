63dc14f785f5b5edcbbfda1d93dce6113d5bd045
Vasilev0009 <ben00000009@gmail.com>
2023-06-08 23:45:11 +0500

    save

--- src/main/java/org/example/Bot.java
+++ src/main/java/org/example/Bot.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendDocument;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.Message;
@@ -15,17 +16,21 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKe
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Bot extends TelegramLongPollingBot {
+    int stage = 0;
     final String bus = "Автобусы";
     final String train = "Пригородные поезда";
     final String all = "Все";
-    final String today = "Все";
+    final String today = "Сегодня";
     final String tomorrow = "Завтра";
     final String everyday = "Ежедневно";
+
+    String url = "http://расписание.рф/автобус/66/Екатеринбург/45/Шадринск/завтра";
 
     public Bot() {
         super("6269475408:AAHHxNe0IUwkqKXHAIBo4Gz1ZbWcbaeA950");
@@ -38,21 +43,35 @@ public class Bot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        String response = null;
         Storage storage = new Storage();
+        ScriptMenu scriptMenu = new ScriptMenu();
         // We check if the update has a message and the message has text
         if (update.hasMessage() && update.getMessage().hasText()) {
             //Извлекаем из объекта сообщение пользователя
             String inMess = update.getMessage().getText();
+            ArrayList inTrains;
             long chatId = update.getMessage().getChatId();
+            scriptMenu.setInMess(inMess, stage);
 
             switch (inMess) {
                 case "/start":
                     transports(chatId);
-                    inMess = null;
+                    stage = 1;
                     break;
                 case "/get":
-                    inMess = storage.getRandQuote();
+                    storage.setUrl(url);
+                    inTrains = storage.getquoteList();
+                    SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
+                    message.setChatId(update.getMessage().getChatId().toString());
+                    for (int i = 0; i < inTrains.size(); ++i) {
+
+                        message.setText((String) inTrains.get(i));
+                        try {
+                            execute(message); // Call method to send the message
+                        } catch (TelegramApiException e) {
+                            e.printStackTrace();
+                        }
+                    }
                     break;
                 case "/test":
                     break;
@@ -63,6 +82,11 @@ public class Bot extends TelegramLongPollingBot {
             SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
             message.setChatId(update.getMessage().getChatId().toString());
             message.setText(inMess);
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
         } else if (update.hasCallbackQuery()) {
             String callbackData = update.getCallbackQuery().getData();
             long messageId = update.getCallbackQuery().getMessage().getMessageId();
@@ -76,23 +100,37 @@ public class Bot extends TelegramLongPollingBot {
                     text = "train";
                     message.setText(text);
                     message.setMessageId((int) messageId);
+                    day(chatId);
                     break;
                 case all:
                     text = "all";
                     message.setText(text);
                     message.setMessageId((int) messageId);
+                    day(chatId);
                     break;
                 case bus:
                     text = "bus";
                     message.setText(text);
                     message.setMessageId((int) messageId);
+                    day(chatId);
+                    break;
+                case today:
+                    destination(chatId);
+                    break;
+                case tomorrow:
+                    destination(chatId);
+                    break;
+                case everyday:
+                    destination(chatId);
                     break;
                 default:
                     text = "Сообщение не распознано";
                     message.setText(text);
                     message.setMessageId((int) messageId);
             }
-                    //Получаем текст сообщения пользователя, отправляем в написанный нами обработчик
+        }
+    }
+    //Получаем текст сообщения пользователя, отправляем в написанный нами обработчик
 /*            String response = parseMessage(inMess.getText());
 
             //Создаем объект будущей клавиатуры и выставляем нужные настройки
@@ -116,50 +154,8 @@ public class Bot extends TelegramLongPollingBot {
             message.setText(response);
             message.setReplyMarkup(replyKeyboardMarkup);
 */
-                    try {
-                        execute(message); // Call method to send the message
-                    } catch (TelegramApiException e) {
-                        e.printStackTrace();
-                    }
-            }
-        }
-        private void day ( long chatId){
-            SendMessage message = new SendMessage();
-            message.setChatId(String.valueOf(chatId));
-            message.setText("Выбирите вид транспора");
 
-            InlineKeyboardMarkup InLineKeyboard = new InlineKeyboardMarkup();
-            List<List<InlineKeyboardButton>> rowsInLine = new ArrayList<>();
-            List<InlineKeyboardButton> rowInLine = new ArrayList<>();
-
-            var trainButton = new InlineKeyboardButton();
-            trainButton.setText("Сегодня");
-            trainButton.setCallbackData(today);
-
-            var busButton = new InlineKeyboardButton();
-            busButton.setText("Завтра");
-            busButton.setCallbackData(tomorrow);
-
-            var allButton = new InlineKeyboardButton();
-            allButton.setText("Ежедневно");
-            allButton.setCallbackData(everyday);
-
-            rowInLine.add(trainButton);
-            rowInLine.add(busButton);
-            rowInLine.add(allButton);
-
-            rowsInLine.add(rowInLine);
-
-            InLineKeyboard.setKeyboard(rowsInLine);
-            message.setReplyMarkup(InLineKeyboard);
-            try {
-                execute(message); // Call method to send the message
-            } catch (TelegramApiException e) {
-                e.printStackTrace();
-            }
-        }
-
-    private void transports ( long chatId){
+    private void day(long chatId) {
         SendMessage message = new SendMessage();
         message.setChatId(String.valueOf(chatId));
         message.setText("Выбирите дату отправления");
@@ -169,16 +165,16 @@ public class Bot extends TelegramLongPollingBot {
         List<InlineKeyboardButton> rowInLine = new ArrayList<>();
 
         var trainButton = new InlineKeyboardButton();
-        trainButton.setText("Пригородные поезда");
-        trainButton.setCallbackData(train);
+        trainButton.setText("Сегодня");
+        trainButton.setCallbackData(today);
 
         var busButton = new InlineKeyboardButton();
-        busButton.setText("Автобусы");
-        busButton.setCallbackData(bus);
+        busButton.setText("Завтра");
+        busButton.setCallbackData(tomorrow);
 
         var allButton = new InlineKeyboardButton();
-        allButton.setText("Все");
-        allButton.setCallbackData(all);
+        allButton.setText("Ежедневно");
+        allButton.setCallbackData(everyday);
 
         rowInLine.add(trainButton);
         rowInLine.add(busButton);
@@ -194,6 +190,59 @@ public class Bot extends TelegramLongPollingBot {
             e.printStackTrace();
         }
     }
+
+    private void transports(long chatId) {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText("Выбирите вид транспорта");
+
+        InlineKeyboardMarkup InLineKeyboard = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rowsInLine = new ArrayList<>();
+        List<InlineKeyboardButton> rowInLine = new ArrayList<>();
+        List<InlineKeyboardButton> rowInLine2 = new ArrayList<>();
+        List<InlineKeyboardButton> rowInLine3 = new ArrayList<>();
+
+        var trainButton = new InlineKeyboardButton();
+        trainButton.setText("Пригородные поезда");
+        trainButton.setCallbackData(train);
+
+        var busButton = new InlineKeyboardButton();
+        busButton.setText("Автобусы");
+        busButton.setCallbackData(bus);
+
+        var allButton = new InlineKeyboardButton();
+        allButton.setText("Все");
+        allButton.setCallbackData(all);
+
+        rowInLine.add(trainButton);
+        rowInLine2.add(busButton);
+        rowInLine3.add(allButton);
+
+        rowsInLine.add(rowInLine);
+        rowsInLine.add(rowInLine2);
+        rowsInLine.add(rowInLine3);
+
+        InLineKeyboard.setKeyboard(rowsInLine);
+        message.setReplyMarkup(InLineKeyboard);
+        try {
+            execute(message); // Call method to send the message
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
     }
+
+    private void destination(long chatId) {
+        SendMessage message = new SendMessage();
+        message.setChatId(String.valueOf(chatId));
+        message.setText("Выбирите станцию назначения");
+        try {
+            execute(message); // Call method to send the message
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
 
 
---
+++ src/main/java/org/example/ScriptMenu.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class ScriptMenu {
+    private String inMess;
+    int stage = 1;
+
+    public void setInMess(String inMess, int stage) {
+        this.inMess = inMess;
+        this.stage = stage;
+        }
+
+}
